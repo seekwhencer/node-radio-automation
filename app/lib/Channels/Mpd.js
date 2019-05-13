913fd4961bd66d1957942276f64fa0697d3e3e87
seekwhencer <seekwhencer>
2019-05-13 21:45:03 +0200

    mpd

--- app/lib/Channels/Mpd.js
+++ app/lib/Channels/Mpd.js
@@ -101,7 +101,7 @@ module.exports = class Mpd extends Module {
         this.process = spawn(this.options.bin, options);
         this.process.stderr.setEncoding('utf8');
         this.process.stderr.on('data', (chunk) => {
-            LOG(this.label, this.name, 'TTY', chunk.trim());
+            //LOG(this.label, this.name, 'TTY', chunk.trim());
             this.emit('data', chunk);
 
             Object.keys(match).forEach((key) => {
@@ -121,7 +121,7 @@ module.exports = class Mpd extends Module {
         });
 
         this.process.stderr.on('end', function () {
-            this.emit('end', this);
+            
         });
     };
 
