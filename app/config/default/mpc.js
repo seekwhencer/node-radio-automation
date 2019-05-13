672ea22532a555593ac47b12727788f81262447e
seekwhencer <seekwhencer>
2019-05-14 00:28:26 +0200

    mpc

--- app/config/default/mpc.js
+++ app/config/default/mpc.js
@@ -1,7 +1,8 @@
 module.exports = {
     bin: "/usr/bin/mpc",
-    query_delay: 100,
-    config : {
-        host:'127.0.0.1'
+    query_delay: 2000,
+    status_repeat_time: 2,
+    config: {
+        host: '127.0.0.1'
     }
 };
