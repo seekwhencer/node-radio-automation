80a40e9039d1c0a6301b86f4ed2e319017da0998
seekwhencer <seekwhencer>
2019-05-29 22:12:20 +0200

    api get show folder

--- app/lib/Api/routes/show/folder.js
+++ app/lib/Api/routes/show/folder.js
@@ -11,10 +11,36 @@ module.exports = class extends RouteSet {
             if (!show)
                 return this.error(`Show not found`, res);
 
-            const folders = FOLDERSYNC( P(`${APP_DIR}/${CONFIG.station.path.audio}/${show.options.path.music}`) );
-            res.json(folders);
+            let folders = FOLDERSYNC(P(`${APP_DIR}/${CONFIG.station.path.audio}`));
+
+            let ii = {};
+            folders.forEach(i => {
+                ii[i] = P(`${APP_DIR}/${CONFIG.station.path.audio}/${i}`);
+            });
+            res.json(ii);
         });
 
+        this.router.get('/:show/folder/:folder', (req, res) => {
+            const folder = req.params.folder;
+            if (!['music', 'intro', 'spot', 'podcast'].includes(folder))
+                return this.error(`Wrong folder type given`, res);
+
+            const show_id = req.params.show;
+            const show = SHOWS.get(show_id, 'id');
+            if (!show)
+                return this.error(`Show not found`, res);
+
+            const folders = FOLDERSYNC(P(`${APP_DIR}/${CONFIG.station.path.audio}/${folder}`));
+
+            if (!folders)
+                return this.error(`No folder found`, res);
+
+            let ii = {};
+            folders.forEach(i => {
+                ii[i] = P(`${APP_DIR}/${CONFIG.station.path.audio}/${folder}/${i}`);
+            });
+            res.json(ii);
+        });
 
         return this.router;
     }
