feac25b730520d91bf1231c146b13e7dffaba9f3
seekwhencer <seekwhencer>
2019-06-08 18:07:33 +0200

    scheduling, scheduling api

--- app/lib/Schedules/Job.js
+++ app/lib/Schedules/Job.js
@@ -7,19 +7,43 @@ module.exports = class ScheduleJob {
         this.name = 'schedulejob';
         this.label = 'SCHEDULE JOB';
         this.schedule = args.schedule;
+        this.channel = this.schedule.channel;
 
         let cronArray = [];
         for (let i = 1; i < 6; i++) {
             cronArray.push(`${this.schedule.options.cron[`${i}`]}`);
         }
         const cronString = cronArray.join(' ');
-        const channelName = this.schedule.channel.options.name;
-        const showName = this.schedule.channel.shows.get(this.schedule.options.show_id, 'id').name;
+        const channelName = this.channel.options.name;
+
+        if (!this.schedule.options.show_id) {
+            LOG(this.label, 'CANT START JOB: no show_id given.');
+            return false;
+        }
+
+        const show = this.channel.shows.get(this.schedule.options.show_id, 'id');
+        if (!show) {
+            LOG(this.label, 'SHOW NOT FOUND: ', this.schedule.options.show_id);
+            return false;
+        }
+
+        const showName = show.name;
         LOG(this.label, 'INIT', cronString, 'CHANNEL:', channelName, 'SHOW:', showName);
 
         return clock.scheduleJob(cronString, () => {
-
-            console.log(this, label, 'TRIGGERING SCHEDULED JOB');
+            LOG('');
+            LOG('/////////');
+            LOG(this.label, 'TRIGGERING:', cronString, 'CHANNEL:', channelName, 'SHOW:', showName);
+            LOG('/////////');
+            LOG('');
+            /**
+             * DOING THINGS HERE
+             *
+             * set the given show for the channel and play it
+             */
+            if (this.schedule.options.show_id)
+                this.channel.setShow(this.schedule.options.show_id, 'id');
+                this.channel.updatePlaylist();
         });
     }
 };
